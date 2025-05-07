eeb253cfb471ee6f19e7379a6695b68158365647
fmarcosdev <fmarcosdev@gmail.com>
2025-05-07 13:34:56 -0400

    item details, months in [es]

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
-const
-  fs = require('fs'),
-  handlebars = require('handlebars'),
-  handlebarsWax = require('handlebars-wax'),
-  addressFormat = require('address-format'),
-  moment = require('moment'),
-  Swag = require('swag');
+const fs = require("fs"),
+  handlebars = require("handlebars"),
+  handlebarsWax = require("handlebars-wax"),
+  addressFormat = require("address-format"),
+  moment = require("moment"),
+  Swag = require("swag");
+require("moment/locale/es");
 
+moment.locale("es");
 Swag.registerHelpers(handlebars);
 
 handlebars.registerHelper({
-
   wrapURL: function (url) {
-    if (url === null || url === '' || url === "") return "";
+    if (url === null || url === "" || url === "") return "";
     let validUrl;
     validUrl = new URL(url);
     const escapedUrl = handlebars.escapeExpression(url);
-    const displayUrl = escapedUrl.replace(/.*?:\/\//g, '');
+    const displayUrl = escapedUrl.replace(/.*?:\/\//g, "");
     const wrappedUrl = `<a href="${escapedUrl}">${displayUrl}</a>`;
     return new handlebars.SafeString(wrappedUrl);
   },
 
   wrapMail: function (address) {
-    const wrappedAddress = '<a href="mailto:' + address + '">' + address + "</a>";
+    const wrappedAddress =
+      '<a href="mailto:' + address + '">' + address + "</a>";
     return new handlebars.SafeString(wrappedAddress);
   },
 
@@ -31,17 +32,15 @@ handlebars.registerHelper({
       city: city,
       subdivision: region,
       postalCode: postalCode,
-      countryCode: countryCode
+      countryCode: countryCode,
     });
-    return addressList.join('<br/>');
+    return addressList.join("<br/>");
   },
 
   formatDate: function (date) {
-    if (moment(date).isValid())
-    {
-      return moment(date).format('MMM YYYY');
-    }
-    else return "Present";
+    if (moment(date).isValid()) {
+      return moment(date).format("MMM YYYY");
+    } else return "Presente";
   },
 
   even: function (index) {
@@ -54,29 +53,29 @@ handlebars.registerHelper({
 
   length: function (array) {
     return array.length;
-  }
+  },
 });
 
 function render(resume) {
   let dir = __dirname,
-    css = fs.readFileSync(dir + '/style.css', 'utf-8'),
-    resumeTemplate = fs.readFileSync(dir + '/resume.hbs', 'utf-8');
+    css = fs.readFileSync(dir + "/style.css", "utf-8"),
+    resumeTemplate = fs.readFileSync(dir + "/resume.hbs", "utf-8");
 
   let Handlebars = handlebarsWax(handlebars);
 
-  Handlebars.partials(dir + '/views/**/*.{hbs,js}');
-  Handlebars.partials(dir + '/partials/**/*.{hbs,js}');
+  Handlebars.partials(dir + "/views/**/*.{hbs,js}");
+  Handlebars.partials(dir + "/partials/**/*.{hbs,js}");
 
   return Handlebars.compile(resumeTemplate)({
     css: css,
-    resume: resume
+    resume: resume,
   });
 }
 
 module.exports = {
   render: render,
   pdfRenderOptions: {
-    format: 'A4',
-    mediaType: 'print',
+    format: "A4",
+    mediaType: "print",
   },
 };
